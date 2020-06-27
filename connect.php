51f7a337d1a2af53c813b2e030f3e7565a9bed1a
PawCyb9809 <48087524+PawCyb9809@users.noreply.github.com>
2020-06-27 03:17:09 +0200

    Uploaded files

---
+++ connect.php
@@ -0,0 +1,8 @@
+<?php
+    $host = "serwer2016433.home.pl";
+	$db_user = "32493616_golemiada";
+	$db_password = "#Ziemniakk19";
+	$db_name = "32493616_golemiada";
+	$polaczenie = new mysqli($host, $db_user, $db_password, $db_name);
+	mysqli_set_charset($polaczenie,"utf8");
+?>
